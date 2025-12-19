2a14c46a69ad916485378e714d6c14c4ca73bee9
bertholdt <122198422+thethien2906@users.noreply.github.com>
2025-12-19 10:10:26 +0700

    link in same tab

--- components/Button.jsx
+++ components/Button.jsx
@@ -8,8 +8,6 @@ const Button = ({ buttonLink, buttonText, className }) => {
     return (
       <a
         href={buttonLink}
-        target="_blank"
-        rel="noopener noreferrer"
         className={clsx(
           "rounded-xl bg-orange-600 px-5 py-4 text-center text-xl font-bold uppercase tracking-wide text-white transition-colors duration-150 hover:bg-orange-700 md:text-2xl",
           className
